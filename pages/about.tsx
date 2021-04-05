45d300e1c94210065ecc84f4adefcbb412ef3831
Wahyudin <whydn.id@hotmail.com>
2021-04-06 04:21:40 +0700

    feature: add about & blog page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Hero from '@components/commons';
+import Layout from '@components/layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Hero
+        title="About Me"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.t"
+      />
+    </Layout>
+  );
+}
